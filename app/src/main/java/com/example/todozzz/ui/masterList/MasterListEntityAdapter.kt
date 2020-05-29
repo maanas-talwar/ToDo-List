5d2856372e2e7a5ec5350231dbf083046439b936
maanas-talwar <maanastalwar1@gmail.com>
2020-05-29 16:51:15 +0530

    Added a layout for each list item with X.

--- app/src/main/java/com/example/todozzz/ui/masterList/MasterListEntityAdapter.kt
+++ app/src/main/java/com/example/todozzz/ui/masterList/MasterListEntityAdapter.kt
@@ -1,14 +1,21 @@
 package com.example.todozzz.ui.masterList
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todozzz.R
 import com.example.todozzz.TaskItemViewHolder
 import com.example.todozzz.database.MasterListEntity
 
-class MasterListEntityAdapter: RecyclerView.Adapter<TaskItemViewHolder> () {
+class MasterListEntityAdapter: RecyclerView.Adapter<MasterListEntityAdapter.ViewHolder> () {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val taskData: TextView = itemView.findViewById(R.id.task_data)
+        val deleteTaskIcon: ImageView = itemView.findViewById(R.id.done_task_icon)
+    }
+
     var data =  listOf<MasterListEntity>()
         set(value) {
             field = value
@@ -16,16 +23,20 @@ class MasterListEntityAdapter: RecyclerView.Adapter<TaskItemViewHolder> () {
         }
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.taskInfo
-
+        val res = holder.itemView.context.resources
+        holder.taskData.text = item.taskInfo
+        holder.deleteTaskIcon.setImageResource(R.drawable.clear_task_icon)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater =
+            LayoutInflater.from(parent.context)
         val view = layoutInflater
-            .inflate(R.layout.task_item_view, parent, false) as TextView
-        return TaskItemViewHolder(view)
+            .inflate(R.layout.list_item_task,
+                parent, false)
+        return ViewHolder(view)
     }
 }
